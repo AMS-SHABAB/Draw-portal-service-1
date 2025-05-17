5e86470c513b65f10f24989fe2dd2337dfdebb56
Muktadir Shabab <muktadirsabab21@gmail.com>
2025-05-17 14:04:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+ const hamburger = document.getElementById('hamburger');
+    const mobileNav = document.getElementById('mobileNav');
+    const navbar = document.getElementById('navbar');
+
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      mobileNav.classList.toggle('show');
+    });
+
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    });
 function toggleDetails(button) {
   const details = button.closest('.card').querySelector('.details');
   const seeDetailsBtn = button;
